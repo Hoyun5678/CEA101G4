217e8cbfb32851846db63111e781b853e55fdb82
Simon ho <hoyun@5678gmail.com>
2021-01-12 22:47:44 +0800

    活動複合查詢

--- CEA101_G4/src/jdbc/util/CompositeQuery/jdbcUtil_CompositeQuery_Activity_Period.java
+++ CEA101_G4/src/jdbc/util/CompositeQuery/jdbcUtil_CompositeQuery_Activity_Period.java
@@ -1,22 +1,27 @@
 package jdbc.util.CompositeQuery;
+
 import java.util.*;
+
 public class jdbcUtil_CompositeQuery_Activity_Period {
 	public static String get_aCondition_For_Oracle(String columnName, String value) {
 
 		String aCondition = null;
 
-		if ("key_word".equals(columnName)) // 用於varchar
-			aCondition = "act_period_id" + " like '%" + value + "%' "+
-		"or act_id" + " like '%" + value + "%' "+
-		"or act_up_limit" + " like '%" + value + "%' "+
-		"or act_low_limit" + " like '%" + value + "%' "+
-		"or act_cur_price" + " like '%" + value + "%' "+
-		"or act_period_status" + " like '%" + value + "%' "+
-		"or act_sign_sum" + " like '%" + value + "%' ";
-		else if ("act_period_start".equals(columnName))                          // 用於Oracle的date
-			aCondition = "to_char(" + columnName + ",'yyyy-mm-dd')='" + value + "'";
+		if ("word".equals(columnName)) {
+			// 用於varchar
 
-		return aCondition + " ";
+			aCondition = "ACT_NAME" + " like '%" + value + "%' " + "or ACT_DES" + " like '%" + value + "%' "
+					+ "or ACT_ADD" + " like '%" + value + "%' " + "or ACT_TYPE_NAME" + " like '%"+ value + "%' ";
+		} else if ("datefilter1".equals(columnName)) {
+
+			aCondition = " ACT_PERIOD_START <" + " to_timestamp('" + value + " 23:59:59', 'yyyy-mm-dd hh24:mi:ss')"
+					+"and ACT_PERIOD_START>to_timestamp('"+value+" 00:00:00', 'yyyy-mm-dd hh24:mi:ss')";
+//			"to_char(" + columnName + ",'yyyy-mm-dd')='" + value + "'";
+
+		}else if("price".equals(columnName)) {
+			aCondition= "ACT_CUR_PRICE <"+value;
+		}
+		return aCondition;
 	}
 
 	public static String get_WhereCondition(Map<String, String[]> map) {
@@ -25,7 +30,7 @@ public class jdbcUtil_CompositeQuery_Activity_Period {
 		int count = 0;
 		for (String key : keys) {
 			String value = map.get(key)[0];
-			if (value != null && value.trim().length() != 0	&& !"action".equals(key)) {
+			if (value != null && value.trim().length() != 0 && !"action".equals(key)) {
 				count++;
 				String aCondition = get_aCondition_For_Oracle(key, value.trim());
 
@@ -37,7 +42,7 @@ public class jdbcUtil_CompositeQuery_Activity_Period {
 				System.out.println("有送出查詢資料的欄位數count = " + count);
 			}
 		}
-		
+
 		return whereCondition.toString();
 	}
 }
